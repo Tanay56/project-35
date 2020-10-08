445c10453cfc5d357d556cbaaa927adf058863fd
Tanay56 <66378096+Tanay56@users.noreply.github.com>
2020-10-08 20:07:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+ 
+}
+
+function draw(){
+  background("white");
+}
